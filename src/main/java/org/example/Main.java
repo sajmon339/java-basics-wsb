3965bf69188eca6f06a234962e32c691df186111
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 16:51:48 +0100

    7 konwerter minuty/godziny

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int liczba = 0;
-        System.out.println( liczba++ );
-        System.out.println( liczba );
-        System.out.println( --liczba );
+        int godziny = 3;
+        int minuty = godziny * 60;
+
+        int minuty2 = 120;
+        int godziny2 = minuty2 / 60;
+
+        System.out.println(godziny + " godziny, to: "  + minuty + " minut.");
+        System.out.println(minuty2 + " minuty, to: "  + godziny2 + " godzin.");
 
     }
 }
