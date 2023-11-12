f393ceb37c4e9ecb294cdc318a034870faeb6cea
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 16:43:06 +0100

    5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int liczba = 57; // deklaracja i inicjalizacja
-        int liczba2; //deklaracja
-        liczba2 = 2;
 
-        System.out.println(liczba  + liczba2);//operacja arytmetyczna (plus jako działanie arytmetyczne)
-        System.out.println(liczba + " \t" + liczba2);//konkatenacja (plus jako konkatenacja)
+        System.out.println( 4 / 2);
+        System.out.println( 4.0 / 2);
+        System.out.println( (float) 4 / 2);
+        System.out.println();
 
     }
 }
