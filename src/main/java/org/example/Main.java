e41622249516084f08d296bcd4e89bc89c1d5e64
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 17:27:24 +0100

    12 sprawdzanie parzystosci

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         int zm = 6;
-        if(zm % 2 == 0) {
-            System.out.println("Liczba: " + zm + " jest parzysta.");
-        }else{
-            System.out.println("Liczba: " + zm + " jest nieparzysta.");
-        }
+        System.out.println(zm % 2 == 0 ? "Liczba: " + zm + " jest parzysta." : "Liczba: " + zm + " jest nieparzysta.");
 
 
     }
