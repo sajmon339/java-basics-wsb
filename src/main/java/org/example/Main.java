3830390674f7a310a63e0f3d3bede7ffe1f50705
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 16:59:36 +0100

    9 konwerter minuty/godziny ulepszenie zapetlenie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj liczbę godzin: [zatwierdź enterem]");
-        int godziny = scan.nextInt();
-        int minuty = godziny * 60;
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj liczbę godzin: [zatwierdź enterem]");
+            int godziny = scan.nextInt();
+            int minuty = godziny * 60;
 
-        System.out.println("Podaj liczbę minut: [zatwierdź enterem]");
-        int minuty2 = scan.nextInt();
-        int godziny2 = minuty2 / 60;
+            System.out.println("Podaj liczbę minut: [zatwierdź enterem]");
+            int minuty2 = scan.nextInt();
+            int godziny2 = minuty2 / 60;
 
-        System.out.println(godziny + " godziny, to: "  + minuty + " minut.");
-        System.out.println(minuty2 + " minuty, to: "  + godziny2 + " godzin.");
+            System.out.println(godziny + " godziny, to: " + minuty + " minut.");
+            System.out.println(minuty2 + " minuty, to: " + godziny2 + " godzin.");
+        }
 
     }
 }
