9bf21e5e76dd59c28f3ff8561e553c5823c658d9
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 17:25:33 +0100

    11 sprawdzanie parzystosci

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        while (true) {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Podaj liczbę godzin: [zatwierdź enterem]");
-            float godziny = scan.nextFloat();
-            float minuty = godziny * 60;
-
-            System.out.println("Podaj liczbę minut: [zatwierdź enterem]");
-            float minuty2 = scan.nextFloat();
-            float godziny2 = minuty2 / 60;
-
-            System.out.println(godziny + " godziny, to: " + Math.ceil(minuty) + " minut.");
-            System.out.println(minuty2 + " minuty, to: " + godziny2 + " godzin.");
+        int zm = 6;
+        if(zm % 2 == 0) {
+            System.out.println("Liczba: " + zm + " jest parzysta.");
+        }else{
+            System.out.println("Liczba: " + zm + " jest nieparzysta.");
         }
+
 
     }
 }
