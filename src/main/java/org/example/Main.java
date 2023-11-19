6373ef197dcfc859b18aa8d2085ad8a2fbbe2cc9
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 17:03:13 +0100

    24

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-        for( int i = 0; i > 10 ; i++ ){
-            System.out.println(i);
+        int[] tab = {25, 5, 5, 6, 8, 9};
+        for(int i = 0; i < tab.length; i++){
+            System.out.println( tab[i] );
         }
+
 
     }
 }
