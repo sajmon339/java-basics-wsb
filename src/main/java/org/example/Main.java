f8e7ce4d5e29ee25386a2eb0d7a1dff60072f6d5
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 15:22:34 +0100

    15 odp zad5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,23 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int a = -1,
-            b = -11,
-            c = 12;
-
-        double delta = Math.pow(b, 2) - 4 * a * c;
-        System.out.println("Delta: " + delta);
-
-        if(delta < 0){
-            System.out.println("Brak pierwiastków");
-        } else if (delta == 0) {
-            double x0 = -b / (2 * a);
-            System.out.println("Jedno miejsce zerowe x0: " + x0);
-        }else if(delta > 0){
-            double x1 = (-b - Math.sqrt(delta)) / (2 * a);
-            double x2 = (-b + Math.sqrt(delta)) / (2 * a);
-            System.out.println("Dwa miejsca zerowe x1: " + x1 + "\nx2: " + x2 );
-        }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj wartość do sprawdzenia: [zatwierdź enterem]");
+        int input = scan.nextInt();
+        System.out.println( input % 2 == 0 ? "Liczba: " + input + " jest parzysta!" :
+                                             "Liczba: " + input + " NIE jest parzysta!");
 
     }
 }
