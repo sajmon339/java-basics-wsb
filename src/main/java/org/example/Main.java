34f4317157d7092abe0df39f92456e04b4753281
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 17:20:48 +0100

    29

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,19 @@
 package org.example;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        //START
-        //Podaj a
-        System.out.println("Podaj a:");
-        int a = scan.nextInt();
-        //Podaj b
-        System.out.println("Podaj b:");
-        int b = scan.nextInt();
-        //Podaj c
-        System.out.println("Podaj c:");
-        int c = scan.nextInt();
-        //Blok operacji:
-        int suma = a + b + c;
-        //Blok wypisz suma:
-        System.out.println(suma);
-        //KONIEC (STOP)
+        scan.useLocale(Locale.US);
+
+        System.out.println("Podaj liczbę z przecinkiem:");
+        float liczba = scan.nextFloat();
+
+        System.out.println("Podana liczba to: " + liczba);
+
 
     }
 }
