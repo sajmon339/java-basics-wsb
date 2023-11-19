1418ce4a93b20b303b9977e0ca74b2f08b7101e2
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 15:57:41 +0100

    19

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,44 +10,22 @@ public class Main {
         int miesiac = scan.nextInt();
 
         switch (miesiac){
-            case 1:
+            case 1 -> {
                 System.out.println("Podaleś numer: " + miesiac + " to jest Styczeń.");
-                break;
-            case 2:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Luty.");
-                break;
-            case 3:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Marzec.");
-                break;
-            case 4:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Kwiecień.");
-                break;
-            case 5:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Maj.");
-                break;
-            case 6:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Czerwiec.");
-                break;
-            case 7:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Lipiec.");
-                break;
-            case 8:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Sierpień.");
-                break;
-            case 9:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Wrzesień.");
-                break;
-            case 10:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Październik.");
-                break;
-            case 11:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Listopad.");
-                break;
-            case 12:
-                System.out.println("Podaleś numer: " + miesiac + " to jest Grudzień.");
-                break;
-            default:
-                System.out.println("Podałeś zły numerek");
+                System.out.println("Podaleś numer: " + miesiac + " to jest Styczeń.");
+            }
+            case 2 -> System.out.println("Podaleś numer: " + miesiac + " to jest Luty.");
+            case 3 -> System.out.println("Podaleś numer: " + miesiac + " to jest Marzec.");
+            case 4 -> System.out.println("Podaleś numer: " + miesiac + " to jest Kwiecień.");
+            case 5 -> System.out.println("Podaleś numer: " + miesiac + " to jest Maj.");
+            case 6 -> System.out.println("Podaleś numer: " + miesiac + " to jest Czerwiec.");
+            case 7 -> System.out.println("Podaleś numer: " + miesiac + " to jest Lipiec.");
+            case 8 -> System.out.println("Podaleś numer: " + miesiac + " to jest Sierpień.");
+            case 9 -> System.out.println("Podaleś numer: " + miesiac + " to jest Wrzesień.");
+            case 10 -> System.out.println("Podaleś numer: " + miesiac + " to jest Październik.");
+            case 11 -> System.out.println("Podaleś numer: " + miesiac + " to jest Listopad.");
+            case 12 -> System.out.println("Podaleś numer: " + miesiac + " to jest Grudzień.");
+            default -> System.out.println("Podałeś zły numerek");
         }
 
 
