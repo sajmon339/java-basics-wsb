1ac50bd6a9f58e9e198aed85aff4af7db90c07c6
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 17:04:04 +0100

    25

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
 
         int[] tab = {25, 5, 5, 6, 8, 9};
-        for(int i = 0; i < tab.length; i++){
-            System.out.println( tab[i] );
+        for(int element: tab){
+            System.out.println( element );
         }
 
 
