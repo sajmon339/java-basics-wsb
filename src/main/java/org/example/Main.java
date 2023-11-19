23bf26dbe583dc27cd7d2df9a713b4db408a6c93
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 17:14:18 +0100

    28

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 0; i < 10; i++){
-            if(i % 2 != 0){
-                System.out.println(i);
-            }
-        }
-
+        Scanner scan = new Scanner(System.in);
+        //START
+        //Podaj a
+        System.out.println("Podaj a:");
+        int a = scan.nextInt();
+        //Podaj b
+        System.out.println("Podaj b:");
+        int b = scan.nextInt();
+        //Podaj c
+        System.out.println("Podaj c:");
+        int c = scan.nextInt();
+        //Blok operacji:
+        int suma = a + b + c;
+        //Blok wypisz suma:
+        System.out.println(suma);
+        //KONIEC (STOP)
 
     }
 }
