b8670c417eb656fa4ac2ad8674efd34ef3c93f3f
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 17:26:43 +0100

    30

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+ /*     Zadanie 13. Kalkulator opłat bankowych.
+        Poproś użytkownika o podanie salda konta bankowego i oblicz opłatę za prowadzenie konta, przyjmując, że opłata wynosi 10% salda.
+        double accountBalance = 5000.0;*/
         Scanner scan = new Scanner(System.in);
-        scan.useLocale(Locale.US);
 
-        System.out.println("Podaj liczbę z przecinkiem:");
-        float liczba = scan.nextFloat();
+        System.out.println("Podaj saldo konta:");
+        float saldo = scan.nextFloat();
 
-        System.out.println("Podana liczba to: " + liczba);
+        System.out.println( "Twoje saldo to: " + saldo +
+                            "\nTwoja opłata (10%) za prowadzenie konta to: " + (saldo * 0.1) );
 
 
     }
