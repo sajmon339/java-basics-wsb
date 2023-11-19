7f8f46f1de71e542c522fd2908346f2688fb3f1c
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-19 16:24:39 +0100

    23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,33 +6,14 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        while(true) {
-            System.out.println("Podaj miesiąc (numerycznie): ");
-            int miesiac = scan.nextInt();
+        System.out.println("Wprowadź imię: ");
+        String imie = scan.next();
 
-            switch (miesiac) {
-                case 1 -> System.out.println("Podaleś numer: " + miesiac + " to jest Styczeń.");
-                case 2 -> System.out.println("Podaleś numer: " + miesiac + " to jest Luty.");
-                case 3 -> System.out.println("Podaleś numer: " + miesiac + " to jest Marzec.");
-                case 4 -> System.out.println("Podaleś numer: " + miesiac + " to jest Kwiecień.");
-                case 5 -> System.out.println("Podaleś numer: " + miesiac + " to jest Maj.");
-                case 6 -> System.out.println("Podaleś numer: " + miesiac + " to jest Czerwiec.");
-                case 7 -> System.out.println("Podaleś numer: " + miesiac + " to jest Lipiec.");
-                case 8 -> System.out.println("Podaleś numer: " + miesiac + " to jest Sierpień.");
-                case 9 -> System.out.println("Podaleś numer: " + miesiac + " to jest Wrzesień.");
-                case 10 -> System.out.println("Podaleś numer: " + miesiac + " to jest Październik.");
-                case 11 -> System.out.println("Podaleś numer: " + miesiac + " to jest Listopad.");
-                case 12 -> System.out.println("Podaleś numer: " + miesiac + " to jest Grudzień.");
-                default -> System.out.println("Podałeś zły numerek");
-            }
-
-            System.out.println("Jeśli chcesz zakończyć działanie programu wpisze \"x\" ");
-            String x = scan.next();
-            if(x.equals("x")) break;
-
+        switch (imie){
+            case "Anna", "Antoni", "Adam" -> System.out.println("Twoje imię to: Anna, Antoni lub Adam");
+            case "Monika", "Magdalena", "Marek" -> System.out.println("Twoje imię to: Monika, Magdalena lub Marek");
+            default -> System.out.println("Inne imię...");
         }
-
-
 
     }
 }
