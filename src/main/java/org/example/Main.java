8aff419f541a785bd20ac296f611f4460d6bf307
Simon Mikolajek <sajmon339@gmail.com>
2024-01-10 22:42:24 +0100

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,8 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Main {
     public static void main(String[] args) {
